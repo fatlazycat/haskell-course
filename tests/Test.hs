4389cbd3e932ecfcaa44ce061027caeacc2c0be1
Graham Berks <graham@fatlazycat.com>
2015-02-16 07:58:46 +0000

    added in tasty rerun

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,9 +1,15 @@
-module Tests where
+module Main where
 
 import           Lecture1Tests
 import           Test.Tasty
+import           Test.Tasty.Ingredients.Rerun
+import           Test.Tasty.Runners
 
-main = defaultMain tests
+main :: IO ()
+main =
+   defaultMainWithIngredients
+     [ rerunningTests [ consoleTestReporter ] ]
+     tests
 
 tests :: TestTree
 tests = testGroup "Lecture Tests" [Lecture1Tests.unitTests]
