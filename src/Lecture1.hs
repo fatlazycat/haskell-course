0f47819776f4d46a467bd01e6f9a1907ed943fc4
Graham Berks <graham@fatlazycat.com>
2015-02-18 17:29:20 +0000

    doubleEveryOther & sumDigits

--- src/Lecture1.hs
+++ src/Lecture1.hs
@@ -7,3 +7,14 @@ toDigitsRev :: Integer -> [Integer]
 toDigitsRev x
   | x <= 0 = []
   | otherwise = (x `mod` 10) : (toDigitsRev (x `div` 10))
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther xs = reverse $ doubleEveryOtherHelper $ reverse xs
+
+doubleEveryOtherHelper :: [Integer] -> [Integer]
+doubleEveryOtherHelper [] = []
+doubleEveryOtherHelper [x] = [x]
+doubleEveryOtherHelper (x:y:xs) = [x,y*2] ++ (doubleEveryOtherHelper xs)
+
+sumDigits :: [Integer] -> Integer
+sumDigits xs = sum $ concatMap toDigits xs
--- tests/Lecture1Tests.hs
+++ tests/Lecture1Tests.hs
@@ -10,5 +10,8 @@ unitTests = testGroup "Unit tests"
     testCase "1234 gives [1,2,3,4]" $ (toDigits 1234) @?= [1,2,3,4],
     testCase "0 gives []" $ (toDigitsRev 0) @?= [],
     testCase "-17 gives []" $ (toDigitsRev (-17)) @?= [],
-    testCase "1234 gives [4,3,2,1]" $ (toDigitsRev 1234) @?= [4,3,2,1]
-    ]
+    testCase "1234 gives [4,3,2,1]" $ (toDigitsRev 1234) @?= [4,3,2,1],
+    testCase "[8,7,6,5] gives [16,7,12,5]" $ (doubleEveryOther []) @?= [16,7,12,5],
+    testCase "[1,2,3] gives [1,4,3]" $ (doubleEveryOther [1,2,3]) @?= [1,4,3],
+    testCase "Sum digitis" $ (sumDigits [16,7,12,5]) @?= 22
+  ]
