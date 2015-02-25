386c21c83042c4227eaf075f1efe9bce17dd8223
Graham Berks <graham@fatlazycat.com>
2015-02-25 17:46:36 +0000

    setup for lecture 2 work

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import           Lecture1.Lecture1Tests
+import           Lecture2.Lecture2Tests
 import           Test.Tasty
 import           Test.Tasty.Ingredients.Rerun
 import           Test.Tasty.Runners
@@ -12,4 +13,7 @@ main =
      tests
 
 tests :: TestTree
-tests = testGroup "Lecture Tests" [Lecture1.Lecture1Tests.unitTests]
+tests = testGroup "Lecture Tests"
+        [Lecture1.Lecture1Tests.unitTests
+       , Lecture2.Lecture2Tests.unitTests
+        ]
--- tests/lecture2/Lecture2Tests.hs
+++ tests/lecture2/Lecture2Tests.hs
@@ -1,1 +1,9 @@
-module Lecture2Tests where
+module Lecture2.Lecture2Tests where
+
+import           Lecture2.LogAnalysis
+import           Test.Tasty
+import           Test.Tasty.HUnit
+
+unitTests :: TestTree
+unitTests = testGroup "Unit tests"
+  [ testCase "True == True" $ True @?= False ]
