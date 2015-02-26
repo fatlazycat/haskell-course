8dca45e47ee863c8182962799008c4f5890257eb
Graham Berks <graham@fatlazycat.com>
2015-02-26 09:17:56 +0000

    parse error message

--- src/lecture2/LogAnalysis.hs
+++ src/lecture2/LogAnalysis.hs
@@ -2,3 +2,20 @@
 module Lecture2.LogAnalysis where
 
 import Lecture2.Log
+import Text.Read
+
+parseMessage :: String -> LogMessage
+parseMessage s = parseMessageWords (words s) s
+
+parseMessageWords :: [String] -> String -> LogMessage
+parseMessageWords ("E":errorString:timeStampString:xs) entireMsg = makeErrorMessage (readMaybe errorString) (readMaybe timeStampString) (unwords xs) entireMsg
+parseMessageWords ("I":timeStampString:xs) entireMsg = makeInfoMessage (readMaybe timeStampString) (unwords xs) entireMsg
+parseMessageWords _ entireMsg = Unknown entireMsg
+
+makeErrorMessage :: Maybe Int -> Maybe Int -> String  -> String -> LogMessage
+makeErrorMessage (Just errorNumber) (Just timeStamp) msg _ = LogMessage (Error errorNumber) timeStamp msg
+makeErrorMessage _ _ _ entireMsg = Unknown entireMsg
+
+makeInfoMessage :: Maybe Int -> String  -> String -> LogMessage
+makeInfoMessage (Just timeStamp) msg _ = LogMessage Info timeStamp msg
+makeInfoMessage  _ _ entireMsg = Unknown entireMsg
--- tests/lecture2/Lecture2Tests.hs
+++ tests/lecture2/Lecture2Tests.hs
@@ -1,9 +1,16 @@
 module Lecture2.Lecture2Tests where
 
+import           Lecture2.Log
 import           Lecture2.LogAnalysis
 import           Test.Tasty
 import           Test.Tasty.HUnit
 
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
-  [ testCase "True == True" $ True @?= False ]
+  [ testCase "Can parse error message" $
+    parseMessage "E 2 562 help help" @?= LogMessage (Error 2) 562 "help help",
+    testCase "Can parse info message" $
+    parseMessage "I 29 la la la" @?= LogMessage Info 29 "la la la",
+    testCase "Can handle unknown messages" $
+    parseMessage "This is not in the right format" @?= Unknown "This is not in the right format"
+  ]
