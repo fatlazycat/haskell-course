ba8f23b13e69300d743cb8aae4ffd4d4feb24ea7
Graham Berks <graham@fatlazycat.com>
2015-03-02 17:23:45 +0000

    homework 3 exercise 1

--- src/Lecture3/Golf.hs
+++ src/Lecture3/Golf.hs
@@ -1,4 +1,13 @@
-module Lecture3.Golf where
+module Lecture3.Golf(skips) where
 
 skips :: [a] -> [[a]]
-skips _ = undefined
+skips xs = [mapForDivisor n xs | n <- [1..(length xs)]]
+
+mapForDivisor :: Int -> [a] -> [a]
+mapForDivisor n xs = map snd $ filter (isDivisor n) (zippedIndex xs)
+
+isDivisor :: Integral a => a -> (a, t) -> Bool
+isDivisor n (x,_) = mod x n == 0
+
+zippedIndex :: (Num a, Enum a) => [b] -> [(a, b)]
+zippedIndex xs = zip [1..] xs
--- tests/Lecture3/Lecture3Tests.hs
+++ tests/Lecture3/Lecture3Tests.hs
@@ -7,7 +7,10 @@ import           Test.Tasty.HUnit
 unitTests :: TestTree
 unitTests = testGroup "Lecture 3 Unit tests"
   [
-    testCase "process ABCD" $ skips "ABCD" @?= ["ABCD", "CD", "C", "D"],
-    testCase "process ABCD" $ skips "ABCD" @?= ["ABCD", "CD", "C", "D"]
-                                               ]
+    testCase "process ABCD" $ skips "ABCD" @?= ["ABCD", "BD", "C", "D"],
+    testCase "process hello!" $ skips "hello!" @?= ["hello!", "el!", "l!", "l", "o", "!"],
+    testCase "process [1]" $ skips [1] @?= [[1]],
+    testCase "process [True, False]" $ skips [True, False] @?= [[True, False], [False]]
+    --testCase "process []" $ skips [] @?= []
+  ]
 
