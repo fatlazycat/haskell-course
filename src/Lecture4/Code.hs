9dc5f69040dcdb50e14a9c58e09081b88470af6b
Graham Berks <graham@fatlazycat.com>
2015-03-09 21:27:39 +0000

    done balance binary tree

--- src/Lecture4/Code.hs
+++ src/Lecture4/Code.hs
@@ -1,4 +1,4 @@
-module Lecture4.Code(fun1, fun2, fun1', fun2') where
+module Lecture4.Code(fun1, fun2, fun1', fun2', foldTree, Tree(Node, Leaf)) where
 
 fun1 :: [Integer] -> Integer
 fun1 [] = 1
@@ -7,7 +7,7 @@ fun1 (x:xs)
      | otherwise = fun1 xs
 
 fun1' :: [Integer] -> Integer
-fun1' = foldr (\x a -> if even x then (x-2)*a else a) 1 
+fun1' = foldr (\x a -> if even x then (x-2)*a else a) 1
 
 fun2 :: Integer -> Integer
 fun2 1 = 0
@@ -17,3 +17,22 @@ fun2 n
 
 fun2' :: Integer -> Integer
 fun2' x = sum . filter even $ takeWhile (/= 1) $ iterate (\n -> if even n then n `div` 2 else 3 * n + 1) x
+
+data Tree a = Leaf
+             | Node Integer (Tree a) a (Tree a)
+   deriving (Show, Eq)
+
+foldTree :: [a] -> Tree a
+foldTree = foldr (\x tree -> insertTree x tree) Leaf
+
+insertTree :: a -> Tree a -> Tree a
+insertTree x Leaf = Node 0 Leaf x Leaf
+insertTree x (Node n treeLeft i treeRight)
+  | hl <= hr = Node (hr+1) (insertTree x treeLeft) i treeRight
+  | hl > hr = Node (hl+1) treeLeft i (insertTree x treeRight)
+  where hl = heightTree treeLeft
+        hr = heightTree treeRight
+
+heightTree :: Tree a -> Integer
+heightTree Leaf = 0
+heightTree (Node n _ _ _) = n
--- tests/Lecture4/Tests.hs
+++ tests/Lecture4/Tests.hs
@@ -4,6 +4,9 @@ import           Lecture4.Code
 import           Test.Tasty
 import           Test.Tasty.HUnit
 
+expectedTree =
+  Node 3 (Node 2 (Node 1 (Node 0 Leaf 'E' Leaf) 'H' Leaf) 'I' (Node 1 (Node 0 Leaf 'A' Leaf) 'D' Leaf)) 'J' (Node 2 (Node 1 (Node 0 Leaf 'C' Leaf) 'F' Leaf) 'G' (Node 0 Leaf 'B' Leaf))
+
 unitTests :: TestTree
 unitTests = testGroup "Lecture 4 Unit tests"
   [
@@ -11,5 +14,6 @@ unitTests = testGroup "Lecture 4 Unit tests"
     testCase "fun1 basic 2" $ fun1 [5,6,7,8,9] @?= fun1' [5,6,7,8,9],
     testCase "fun1 basic 3" $ fun1 [11,34,1,56,89] @?= fun1' [11,34,1,56,89],
     testCase "fun2 basic 1" $ fun2 68 @?= fun2' 68,
-    testCase "fun2 basic 2" $ fun2 69 @?= fun2' 69
+    testCase "fun2 basic 2" $ fun2 69 @?= fun2' 69,
+    testCase "binary tree" $ foldTree "ABCDEFGHIJ" @?= expectedTree
   ]
