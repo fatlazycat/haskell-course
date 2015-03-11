c8123b30b39b4c48a3040bd246decb04306a6d90
Graham Berks <graham@fatlazycat.com>
2015-03-11 21:35:08 +0000

    setup test case for eval

--- src/Lecture5/Calc.hs
+++ src/Lecture5/Calc.hs
@@ -1,3 +1,6 @@
 module Lecture5.Calc where
 
 import Lecture5.ExprT
+
+eval :: ExprT -> Integer
+eval _ = undefined
--- tests/Lecture5/Tests.hs
+++ tests/Lecture5/Tests.hs
@@ -1,11 +1,12 @@
 module Lecture5.Tests where
 
 import           Lecture5.Calc
+import           Lecture5.ExprT
 import           Test.Tasty
 import           Test.Tasty.HUnit
 
 unitTests :: TestTree
 unitTests = testGroup "Lecture 5 Unit tests"
   [
-    testCase "basic" $ 1 @?= 1
+    testCase "calc to 20" $ eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) @?= 20
   ]
