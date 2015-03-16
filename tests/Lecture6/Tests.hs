91b77e0d94c8eb5d318edde31830f49aa2c147c8
Graham Berks <graham@fatlazycat.com>
2015-03-16 15:49:41 +0000

    h6 e1

--- tests/Lecture6/Tests.hs
+++ tests/Lecture6/Tests.hs
@@ -7,5 +7,7 @@ import           Test.Tasty.HUnit
 unitTests :: TestTree
 unitTests = testGroup "Lecture 6 Unit tests"
   [
-    testCase "fib 1" $ fib 1 @?= 1 
+    testCase "fib 0" $ fib 0 @?= 0, 
+    testCase "fib 1" $ fib 1 @?= 1, 
+    testCase "fib 5" $ fib 5 @?= 5 
   ] 
