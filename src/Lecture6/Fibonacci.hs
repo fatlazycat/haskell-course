83aeb89d62ba09780dd47dce9cc375a4454a2567
Graham Berks <graham@fatlazycat.com>
2015-03-19 06:28:26 +0000

    code doesn't work for stream fibs yet

--- src/Lecture6/Fibonacci.hs
+++ src/Lecture6/Fibonacci.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Lecture6.Fibonacci where
 
 fib :: Integer -> Integer
@@ -5,8 +6,8 @@ fib 0 = 0
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 
-fibs1 :: [Integer]
-fibs1 = [fib n | n <- [0..]]
+fiba' :: [Integer]
+fiba' = [fib n | n <- [0..]]
 
 fib2 :: Int -> Integer
 fib2 = (map fib2' [0..] !!) where
@@ -14,33 +15,50 @@ fib2 = (map fib2' [0..] !!) where
   fib2' 1 = 1
   fib2' n = fib2 (n-1) + fib2(n-2)
 
-fibs2 :: [Integer]
-fibs2 = [fib2 n | n <- [0..]]
+fibb' :: [Integer]
+fibb' = [fib2 n | n <- [0..]]
 
 data Stream a = Node a (Stream a)
 
 streamToList :: Stream a -> [a]
-streamToList (Node x s) = x : streamToList s
+streamToList (Node a s) = a : streamToList s
 
 instance Show a => Show (Stream a) where
   show s = show $ take 20 (streamToList s)
 
 streamRepeat :: a -> Stream a
-streamRepeat x = Node x (streamRepeat x)
+streamRepeat a = Node a (streamRepeat a)
 
 streamMap :: (a -> b) -> Stream a -> Stream b
-streamMap fn (Node x s) = Node (fn x) (streamMap fn s)
+streamMap fn (Node a s) = Node (fn a) (streamMap fn s)
 
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed fn x = Node x' (streamFromSeed fn x')
-  where x' = fn x
+streamFromSeed fn a = Node a' (streamFromSeed fn a')
+  where a' = fn a
 
 nats :: Stream Integer
 nats = streamFromSeed (+1) (-1)
 
 ruler :: Stream Integer
 ruler = foldr1 interleaveStreams (map streamRepeat [0..])
---ruler = undefined
 
 interleaveStreams :: Stream a -> Stream a -> Stream a
-interleaveStreams (Node x stream) otherStream = Node x (interleaveStreams otherStream stream)
+interleaveStreams (Node a stream) otherStream = Node a (interleaveStreams otherStream stream)
+
+x :: Stream Integer
+x = Node 0 (Node 1 (streamRepeat 0))
+
+instance Num (Stream Integer) where
+  fromInteger a = Node a (streamRepeat 0)
+  negate = streamMap negate
+  (+) (Node a a') (Node b b') = Node (a+b) (a' + b')
+  (*) (Node a a') (Node b b') = Node (a*b) (streamRepeat 0) + (Node 0 (streamMap (*a) b') + (a' * Node b b'))
+  abs = streamMap abs
+  signum _ = 1
+
+instance Fractional (Stream Integer) where
+  (/) (Node a a') (Node b b') =
+    Node (div a b) (streamRepeat 0) + Node 0 (streamMap (`div` b) (a' - (Node a a' / Node b b')*b'))
+
+fibs3 :: Stream Integer
+fibs3 = x / (1 - x - x^2 )
--- tests/Lecture6/Tests.hs
+++ tests/Lecture6/Tests.hs
@@ -34,4 +34,6 @@ unitTests = testGroup "Lecture 6 Unit tests"
               (streamMap (+1) (streamRepeat 1))) @?= ([1,2,1,2,1,2] :: [Int]),
 
     testCase "ruler" $ takeFromStream 20 ruler @?= [0,1,0,2,0,1,0,3,0,1,0,2,0,1,0,4,0,1,0,2]
+
+--    testCase "fibs3" $ takeFromStream 5 fibs3 @?= [0,1,1,2,3]
   ]
