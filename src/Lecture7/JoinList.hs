c05fd077c851c2550a1712bec9b967a54af8465f
Graham Berks <graham@fatlazycat.com>
2015-03-25 18:32:51 -0400

    h7 e1

--- src/Lecture7/JoinList.hs
+++ src/Lecture7/JoinList.hs
@@ -1,6 +1,17 @@
 module Lecture7.JoinList where
 
+import Data.Monoid
+
 data JoinList m a = Empty
                    | Single m a
                    | Append m (JoinList m a) (JoinList m a)
    deriving (Eq, Show)
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) x Empty = x
+(+++) left right = Append (mappend (tag left) (tag right)) left right
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty = mempty
+tag (Single m _) = m
+tag (Append m _ _) = m
--- tests/Lecture7/Tests.hs
+++ tests/Lecture7/Tests.hs
@@ -1,11 +1,18 @@
 module Lecture7.Tests where
 
+import           Data.Monoid
 import           Lecture7.JoinList
 import           Test.Tasty
 import           Test.Tasty.HUnit
 
+aJoinList = Single (Product (1 :: Integer)) 'a'
+aJoinList2 = Single (Product (2 :: Integer)) 'b'
+anAppendedJoinList = Append (mappend (Product 1) (Product 2)) aJoinList aJoinList2
+
 unitTests :: TestTree
 unitTests = testGroup "Lecture 7 Unit tests"
   [
-    testCase "1=1" $ 1 @?= 1
+    testCase "x and empty" $ (aJoinList +++ Empty) @?= aJoinList,
+    testCase "+++ some join lists" $ (aJoinList +++ aJoinList2) @?= anAppendedJoinList
+
   ]
