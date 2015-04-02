203ba2ee0414f3eae468a4ec911445a4c7262032
Graham Berks <graham@fatlazycat.com>
2015-04-02 16:52:49 +0100

    tidied up test to have comma at front
    ugly but missing comma at end driving me nuts

--- tests/Lecture7/Tests.hs
+++ tests/Lecture7/Tests.hs
@@ -34,30 +34,31 @@ bufferStringReplaced = fromString "hi\nreplaced\nsomeone\n"
 unitTests :: TestTree
 unitTests = testGroup "Lecture 7 Unit tests"
   [
-    testCase "x and empty" $ (aJoinList +++ Empty) @?= aJoinList,
-    testCase "+++ some join lists" $ (aJoinList +++ aJoinList2) @?= anAppendedJoinList,
+    testCase "x and empty" $ (aJoinList +++ Empty) @?= aJoinList
+  , testCase "+++ some join lists" $ (aJoinList +++ aJoinList2) @?= anAppendedJoinList
 
-    testCase "index 0 == 1" $ indexJ 0 testData @?= Just 1,
-    testCase "index 3 == 4" $ indexJ 3 testData @?= Just 4,
-    testCase "index 4 == 5" $ indexJ 4 testData @?= Just 5,
-    testCase "index 7 == 8" $ indexJ 7 testData @?= Just 8,
-    testCase "with odd data" $ indexJ 4 oddTestData @?= Just 5,
+  , testCase "index 0 == 1" $ indexJ 0 testData @?= Just 1
+  , testCase "index 3 == 4" $ indexJ 3 testData @?= Just 4
+  , testCase "index 4 == 5" $ indexJ 4 testData @?= Just 5
+  , testCase "index 7 == 8" $ indexJ 7 testData @?= Just 8
+  , testCase "with odd data" $ indexJ 4 oddTestData @?= Just 5
 
-    testCase "q == 10" $ score 'q' @?= Score 10,
-    testCase "Q == 10" $ score 'q' @?= Score 10,
-    testCase " == 10" $ score ' ' @?= Score 0,
-    testCase "! == 10" $ score '!' @?= Score 0,
+  , testCase "q == 10" $ score 'q' @?= Score 10
+  , testCase "Q == 10" $ score 'q' @?= Score 10
+  , testCase " == 10" $ score ' ' @?= Score 0
+  , testCase "! == 10" $ score '!' @?= Score 0
 
-    testCase "scoreLine" $
+  , testCase "scoreLine" $
              scoreLine "yay " +++ scoreLine "haskell!" @?=
              Append (Score 23)
                (Single (Score 9) "yay ")
-               (Single (Score 14) "haskell!"),
+               (Single (Score 14) "haskell!")
 
-    testCase "toString" $ toString bufferTestData @?= "hi\nthere\nsomeone\n",
-    testCase "fromString" $ toString bufferString @?= "hi\nthere\nsomeone\n"
+  , testCase "toString" $ toString bufferTestData @?= "hi\nthere\nsomeone\n"
+  , testCase "fromString" $ toString bufferString @?= "hi\nthere\nsomeone\n"
   , testCase "replaceLine" $ toString (replaceLine 1 "replaced" bufferString) @?= toString (bufferStringReplaced)
   , testCase "number of lines" $ numLines bufferString @?= 3
+  , testCase "value" $ value bufferString @?= 22
   ]
 
 calc :: (Sized b, Monoid b) => JoinList b a -> JoinList b a -> b
