6e1d271a421bfb0e15c53319bf14f1a9b2cdb370
Graham Berks <graham@fatlazycat.com>
2015-04-07 17:16:37 +0100

    done h8 ex1 p2

--- src/Lecture8/Party.hs
+++ src/Lecture8/Party.hs
@@ -1,1 +1,12 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Lecture8.Party where
+
+import Lecture8.Employee
+import Data.Monoid
+
+glCons :: Employee -> GuestList -> GuestList
+glCons e (GL employees fun) = GL (e:employees) (empFun e + fun)
+
+instance Monoid GuestList where
+  mempty = GL [] 0
+  mappend (GL xs1 n1) (GL xs2 n2) = GL (xs1 ++ xs2) (n1+n2)
--- tests/Lecture8/Tests.hs
+++ tests/Lecture8/Tests.hs
@@ -1,10 +1,22 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 module Lecture8.Tests where
 
+import           Data.Monoid
+import           Lecture8.Employee
+import           Lecture8.Party
 import           Test.Tasty
 import           Test.Tasty.HUnit
 
 unitTests :: TestTree
 unitTests = testGroup "Lecture 8 Unit tests"
   [
-    testCase "1==1" $ 1 @?= 1
+    testCase "Add one to guest list" $ glCons emp1 (GL [] 0) @?= GL [emp1] 1
+  , testCase "Add one to an existing guest list" $ glCons emp1 (GL [emp2] 2) @?= GL [emp1, emp2] 3
+
+  , testCase "Use of guest list monoid" $ (mappend gl1 gl2) @?= GL [emp1, emp2] 3
   ]
+
+emp1 = Emp "Emp1" 1
+emp2 = Emp "Emp2" 2
+gl1 = GL [emp1] 1
+gl2 = GL [emp2] 2
