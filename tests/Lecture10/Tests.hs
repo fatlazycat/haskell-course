e19987b51a0ea4740b8f140e4b5f670f2ceffb8a
Graham Berks <graham@fatlazycat.com>
2015-04-14 17:11:23 +0100

    test for applicative

--- tests/Lecture10/Tests.hs
+++ tests/Lecture10/Tests.hs
@@ -1,5 +1,6 @@
 module Lecture10.Tests where
 
+import           Control.Applicative
 import           Lecture10.AParser
 import           Test.Tasty
 import           Test.Tasty.HUnit
@@ -8,6 +9,15 @@ unitTests :: TestTree
 unitTests = testGroup "Lecture 10 Unit tests"
   [
     testCase "fmap for Parser" $ runParser (fmap (+ 1) posInt) "123 4" @?= Just(124, " 4")
---  , testCase "Applicate pure Parser" $ (pure 1 :: Parser Integer) @?= Just(1, []) 
+  , testCase "applicative" $ runParser (TwoChar <$> parserA <*> parserB) "AB" @?= Just((TwoChar 'A' 'B'), [])
   ]
 
+isA x = 'A' == x
+isB x = 'B' == x
+
+parserA = satisfy isA
+parserB = satisfy isB
+
+data TwoChar = TwoChar Char Char
+  deriving(Show, Eq)
+          
