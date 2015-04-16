f68b89d9dd79b3a1c26231a14154f438849adc86
Graham Berks <graham@fatlazycat.com>
2015-04-16 19:30:47 +0100

    added even more test

--- tests/Lecture11/Tests.hs
+++ tests/Lecture11/Tests.hs
@@ -12,6 +12,8 @@ unitTests :: TestTree
 unitTests = testGroup "Lecture 11 Unit tests"
   [
     testCase "zeroOrMore multiple" $ runParser (zeroOrMore (satisfy isUpper)) "ABCdEfgH" @?= Just("ABC","dEfgH")
-  , testCase "zeroOrMore multiple" $ runParser (oneOrMore (satisfy isUpper)) "ABCdEfgH" @?= Just("ABC","dEfgH")
+  , testCase "oneOrMore multiple" $ runParser (oneOrMore (satisfy isUpper)) "ABCdEfgH" @?= Just("ABC","dEfgH")
+  , testCase "zeroOrMore multiple" $ runParser (zeroOrMore (satisfy isUpper)) "abcdeFGh" @?= Just("","abcdeFGh")
+  , testCase "oneOrMore multiple" $ runParser (zeroOrMore (satisfy isUpper)) "abcdeFGh" @?= Nothing
   ]
 
