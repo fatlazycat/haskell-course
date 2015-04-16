16fda0ae0e81fe99a08fa8605bb2600a80272e81
Graham Berks <graham@fatlazycat.com>
2015-04-16 19:25:22 +0100

    added tests

--- tests/Lecture11/Tests.hs
+++ tests/Lecture11/Tests.hs
@@ -1,14 +1,17 @@
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 module Lecture11.Tests where
 
-import           Control.Applicative
+import           Control.Applicative()
+import           Data.Char
 import           Lecture11.AParser
+import           Lecture11.SExpr
 import           Test.Tasty
 import           Test.Tasty.HUnit
 
 unitTests :: TestTree
-unitTests = testGroup "Lecture 10 Unit tests"
+unitTests = testGroup "Lecture 11 Unit tests"
   [
-    testCase "1 == 1" $ 1 @?= 1
+    testCase "zeroOrMore multiple" $ runParser (zeroOrMore (satisfy isUpper)) "ABCdEfgH" @?= Just("ABC","dEfgH")
+  , testCase "zeroOrMore multiple" $ runParser (oneOrMore (satisfy isUpper)) "ABCdEfgH" @?= Just("ABC","dEfgH")
   ]
 
