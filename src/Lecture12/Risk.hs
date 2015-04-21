b1b02c716235446f406e8514a1c39fd56b8a3a2e
Graham Berks <graham@fatlazycat.com>
2015-04-21 17:35:34 +0100

    calculated battle, now for monad

--- src/Lecture12/Risk.hs
+++ src/Lecture12/Risk.hs
@@ -2,12 +2,15 @@
 
 module Lecture12.Risk where
 
-import Control.Monad.Random
+import           Control.Monad
+import           Control.Monad.Random
+import           Data.List
+import Data.Map
 
 ------------------------------------------------------------
 -- Die values
 
-newtype DieValue = DV { unDV :: Int } 
+newtype DieValue = DV { unDV :: Int }
   deriving (Eq, Ord, Show, Num)
 
 first :: (a -> b) -> (a, c) -> (b, c)
@@ -20,9 +23,36 @@ instance Random DieValue where
 die :: Rand StdGen DieValue
 die = getRandom
 
+dice :: Int -> Rand StdGen [DieValue]
+dice n = replicateM n die
+
 ------------------------------------------------------------
 -- Risk
 
 type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+  deriving (Show, Eq, Ord)
+
+encounter :: DieValue -> DieValue -> Char
+encounter a d = if a > d then 'a' else 'd'
+
+-- battle :: Battlefield -> Rand StdGen Battlefield
+-- battle b = do
+--   attackersDie <- dice (attackers b)
+--   defendersDie <- dice (defenders b)
+--   return (Battlefield 1 1)
+
+processBattle :: [DieValue] -> [DieValue] -> Battlefield
+processBattle unsortedAttackers unsortedDefenders =
+  Battlefield (updatedCount sortedAttackers results 'a') (updatedCount sortedDefenders results 'd')
+  where results = frequency outcomes
+        outcomes = zipWith encounter sortedAttackers sortedDefenders
+        sortedAttackers = sortBy (flip compare) unsortedAttackers
+        sortedDefenders = sortBy (flip compare) unsortedDefenders
+
+frequency :: (Ord a) => [a] -> Map a Int
+frequency xs = fromListWith (+) [(x, 1) | x <- xs]
+
+updatedCount :: [DieValue] -> Map Char Int -> Char -> Int
+updatedCount rolls m t = (length rolls) - (m ! t)
--- tests/Lecture12/Tests.hs
+++ tests/Lecture12/Tests.hs
@@ -8,5 +8,9 @@ import           Test.Tasty.HUnit
 unitTests :: TestTree
 unitTests = testGroup "Lecture 12 Unit tests"
   [
-    testCase "1 == 1" $ 1 @?= 1
+    testCase "a wins" $ encounter 2 1 @?= 'a'
+  , testCase "b wins with draw" $ encounter 1 1 @?= 'd'
+  , testCase "b wins" $ encounter 1 2 @?= 'd'
+
+  , testCase "processBattle" $ processBattle [DV 1, DV 3, DV 1] [DV 1, DV 1] @?= Battlefield 2 1
   ]
